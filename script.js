b732f478aa1e3a56fda78e02727d50cb0d0ecfc1
Mateusz Choma <kontakt@coderoad.pl>
2020-06-08 07:17:32 +0200

    Renderin form

--- script.js
+++ script.js
@@ -51,12 +51,43 @@ const renderTasksList = function(tasks){
     return container
 }
 
+const renderNewTaskInput = function(){
+    const input = document.createElement('input')
+    input.className = 'todo-list__input'
+    
+    return input
+}
+
+const renderNewTaskButton = function(label){
+    const button = document.createElement('button')
+    button.className = 'todo-list__button'
+
+    button.innerText = label
+
+    return button
+}
+
+const renderNewTaskForm = function(){
+    const container  = document.createElement('form')
+    container.className = 'todo-list__form'
+
+    const inputElement = renderNewTaskInput()
+    const buttonElement = renderNewTaskButton('ADD')
+
+    container.appendChild(inputElement)
+    container.appendChild(buttonElement)
+
+    return container
+}
+
 const render = function() {
     const container  = document.createElement('div')
     container.className = 'todo-list'
 
+    const newTaskFormElement = renderNewTaskForm()
     const taskListElement = renderTasksList(tasks)
 
+    container.appendChild(newTaskFormElement)
     container.appendChild(taskListElement)
 
     return container
